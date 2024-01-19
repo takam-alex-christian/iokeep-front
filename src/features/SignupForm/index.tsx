e6fad8d5e1deb648414eabd3e978d2eb42b1fc2d
takam-alex-christian <takamalexchristian@gmail.com>
2024-01-19 06:31:50 -0800

    improving accessibility and user experience of form components

--- src/features/SignupForm/index.tsx
+++ src/features/SignupForm/index.tsx
@@ -1,31 +1,78 @@
-import { Input, Button, Link } from "@nextui-org/react"
+"use client"
+
+import React, { useState, useRef} from "react"
+import { Input, Button, Link, user } from "@nextui-org/react"
+
+interface FormState {
+    username: string,
+    password: string,
+    confirmPassword: string
+}
 
 export default function () {
+
+
+
+    const [formState, setFormState] = useState<FormState>({
+        username: "",
+        password: "",
+        confirmPassword: ""
+    })
+    
+    function formSubmitHandler(e: React.FormEvent){
+        e.preventDefault()
+        console.log("sigup form submitted")
+    }
     return (
         <div className="p-6 w-96">
-            <form >
+            <form onSubmit={formSubmitHandler}>
                 <div className="flex flex-col gap-4">
                     <div className="flex flex-col gap-2">
                         <Input
+                            key={0}
+
+                            value={formState.username}
+                            onValueChange={(usernameValue) => { setFormState((prevState: FormState) => { return { ...prevState, username: usernameValue } }) }}
+
                             placeholder={"Username"}
                             required={true}
                             size="sm"
+
+                            autoFocus={true}
+                            autoComplete={"off"}
                         />
                         <Input
+                            key={1}
+                            value={formState.password}
+                            onValueChange={(passwordValue) => { setFormState((prevState: FormState) => { return { ...prevState, password: passwordValue } }) }}
+
                             placeholder={"Password"}
+
+                            type="password"
                             required={true}
                             size="sm"
+
+                            autoComplete="off"
                         />
 
                         <Input
+                            key={2}
+
+                            value={formState.confirmPassword}
+                            onValueChange={(confirmPasswordValue) => { setFormState((prevState: FormState) => { return { ...prevState, confirmPassword: confirmPasswordValue } }) }}
+
                             placeholder={"Confirm Password"}
+
+                            type={"password"}
                             required={true}
                             size="sm"
+
+                            autoComplete="off"
                         />
 
                     </div>
 
-                    <Button size="lg" color="primary" variant="solid">Sign Up</Button>
+                    <Button type="submit" size="lg" color="primary" variant="solid">Sign Up</Button>
 
                 </div>
             </form>
