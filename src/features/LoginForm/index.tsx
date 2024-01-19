a68ac9916d7837f821c641c0db37031b7df98dd9
takam-alex-christian <takamalexchristian@gmail.com>
2024-01-19 06:54:34 -0800

    improving and accessibility and user experience of Login form

--- src/features/LoginForm/index.tsx
+++ src/features/LoginForm/index.tsx
@@ -1,25 +1,79 @@
+"use client"
+
+import React, {useEffect, useState} from "react"
 
 import { Input, Button, Link } from "@nextui-org/react"
 
+interface FormState {
+    username: string,
+    password: string
+}
 
 export default function () {
+
+    const [formState, setFormState] = useState<FormState>({
+        username: "",
+        password: ""
+    })
+
+
+    //test code
+    useEffect(()=>{
+        console.log(formState)
+    }, [formState])
+
+    function formSubmitHandler(e: React.FormEvent){
+        e.preventDefault()
+        console.log("Log in Form")
+    }
     return (
         <div className="p-6 w-96 flex flex-col gap-0">
-            <form >
+            <form onSubmit={formSubmitHandler}>
                 <div className="flex flex-col gap-4">
                     <div className="flex flex-col gap-2">
                         <Input
+                            key={0}
+                            
+                            value={formState.username}
+                            onValueChange={(usernameValue)=>{
+                                setFormState((prevFormState: FormState)=>{
+                                    return {...prevFormState, username: usernameValue}
+                                })
+                            }}
+                            
+                            type="text"
+                            name="username"
+                            
+
                             placeholder={"Username"}
                             required={true}
                             size="sm"
+                            
+                            autoComplete="username"
+
                         />
                         <div className="flex flex-row justify-end pt-2 pb-0">
                             <Link href={"#"}>password forgoten ?</Link>
                         </div>
                         <Input
+
+                            key={1}
+
+                            value={formState.password}
+                            onValueChange={(passwordValue)=>{
+                                setFormState((prevFormState: FormState)=>{
+                                    return {...prevFormState, password: passwordValue}
+                                })
+                            }}
+                            
+                            type={"password"}
+                            name="password"
+
                             placeholder={"Password"}
                             required={true}
                             size="sm"
+
+                            autoComplete="password"
                         />
                     </div>
 
