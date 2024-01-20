76a07b3f264124b4ca82bc4bd6d48377533d3562
takam-alex-christian <takamalexchristian@gmail.com>
2024-01-20 03:18:54 -0800

    installing and setting up fontawesome

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,11 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
+
+
 import { NextUiProviderWrapper } from './providers'
 
 const inter = Inter({ subsets: ['latin'] })
