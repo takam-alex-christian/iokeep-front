4cc25045bc35261bd4d123ba21d2fc1959cda156
takam-alex-christian <takamalexchristian@gmail.com>
2024-01-20 03:19:56 -0800

    app page layout built and left to be populated

---
+++ src/app/app/page.tsx
@@ -0,0 +1,25 @@
+import Footer from "@/layouts/Footer";
+import Navbar from "@/layouts/Navbar";
+
+
+export default function () {
+
+    return (
+        <main className="flex flex-col gap-6 min-h-screen">
+            <Navbar />
+            <div className="flex-grow flex flex-row justify-center">
+
+                <div className="w-4/5 flex flex-row gap-8">
+                    <div className=" w-1/5">
+                        sdf
+                    </div>
+
+                    <div className="bg-slate-50 flex-grow">
+                        sdf
+                    </div>
+                </div>
+            </div>
+            <Footer />
+        </main>
+    )
+}
