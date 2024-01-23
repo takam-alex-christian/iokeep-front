a995ef3b0a94457617014e58a2ce84ccbb765c0b
takam-alex-christian <takamalexchristian@gmail.com>
2024-01-23 02:19:44 -0800

    updated app page to incorporate NoteEditor Feature

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,14 +1,14 @@
 import FolderManager from "@/features/FolderManager";
+import NoteEditor from "@/features/NoteEditor"
 import Footer from "@/layouts/Footer";
 import Navbar from "@/layouts/Navbar";
-
 
 
 
 export default function () {
 
     return (
-        <main className="flex flex-col gap-6 min-h-screen">
+        <main className="flex flex-col gap-6 min-h-screen ">
             <Navbar />
             <div className="flex-grow flex flex-row justify-center">
 
@@ -16,8 +16,15 @@ export default function () {
                     <div className=" w-1/5">
                         <FolderManager />
                     </div>
-                    <div className="bg-slate-50 flex-grow">
-                        sdf
+                    <div className="flex flex-row gap-2 flex-grow">
+                        
+                        <div className="w-2/5 bg-red-100">
+                            {/* noteManager */}
+                            noteManager
+                        </div>
+                        <div className="flex-grow w-3/5 flex flex-col ">
+                            <NoteEditor />
+                        </div>
                     </div>
                 </div>
             </div>
