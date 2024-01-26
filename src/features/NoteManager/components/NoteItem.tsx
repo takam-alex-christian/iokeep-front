4c0a6fa9a0ee87c60974c7599454c52b94bc7e9d
takam-alex-christian <takamalexchristian@gmail.com>
2024-01-26 10:41:23 +0800

    implementing NoteItem components to be used in the noteManager feature

---
+++ src/features/NoteManager/components/NoteItem.tsx
@@ -0,0 +1,35 @@
+"use client"
+
+import { Button, ListboxItem } from "@nextui-org/react"
+
+type NoteItemProps = {
+    noteId: string,
+    description: string[],
+    creationDate: {
+        day: number | string, //better be a number
+        month: number | string,
+        year: number | string,
+        hour: number | string,
+        minute: number | string,
+        second: number | string
+    }
+}
+
+export default function NoteItem(props: NoteItemProps) {
+
+    return (
+        <Button key={props.noteId} className="flex flex-row justify-start text-left h-fit p-0 w-full bg-transparent" >
+            <div className="flex flex-col gap-0 p-3 rounded-lg bg-none w-full ">
+                
+                <div className="text-base py-0 font-semibold text-default-600 overflow-hidden overflow-ellipsis">{props.description[0]}</div>
+                
+
+                <div className="flex flex-row gap-3 text-default-400">
+                    <p className="text-default-500 ">{props.creationDate.day}/{props.creationDate.month}/{props.creationDate.year}</p>
+                    <p className="overflow-hidden overflow-ellipsis flex-grow"> {props.description[1]}</p>
+                </div>
+            </div>
+            
+        </Button>
+        )
+}
