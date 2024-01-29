0d1a839a20d1663a6a1fbff525163958f25bcbb6
takam-alex-christian <takamalexchristian@gmail.com>
2024-01-29 15:42:35 +0800

    liveDataReducer implemented and exported for use

---
+++ src/lib/liveDataReducer.ts
@@ -0,0 +1,16 @@
+
+
+import {LiveDataState, LiveDataDispatchAction} from "@/types"
+
+export default function liveDataReducer(prevState: LiveDataState, action: LiveDataDispatchAction): LiveDataState{
+
+    switch (action.type){
+        case "changedSelectedFolder": {
+            return {...prevState, selectedFolderId: action.payload.folderId}
+        }
+        case "changedSelectedNote": {
+            return {...prevState, selectedNoteId: action.payload.noteId}
+        }
+        default: return prevState
+    }
+}
