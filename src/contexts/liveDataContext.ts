b19ab13c1d52836d1615acd15c05fb13afddce29
takam-alex-christian <takamalexchristian@gmail.com>
2024-01-29 15:42:14 +0800

    live data context created and exported

---
+++ src/contexts/liveDataContext.ts
@@ -0,0 +1,21 @@
+
+
+import {createContext, Dispatch} from "react"
+
+import type {LiveDataState, LiveDataDispatchAction} from "@/types"
+
+const initialLiveData: LiveDataState = {
+    selectedFolderId: null,
+    selectedNoteId: null
+}
+
+const liveDataContext = createContext<{
+    liveAppData: LiveDataState,
+    liveAppDataDispatch: Dispatch<LiveDataDispatchAction>
+}>({
+    liveAppData: initialLiveData,
+    liveAppDataDispatch: ()=>{}
+})
+
+
+export {liveDataContext, initialLiveData}
