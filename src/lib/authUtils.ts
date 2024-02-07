94ae7dd938eaa7a29cd059a2b83f4369b3bf4eaa
takam-alex-christian <takamalexchristian@gmail.com>
2024-02-07 19:21:09 +0800

    loginRequest and signupRequest implemented

--- src/lib/authUtils.ts
+++ src/lib/authUtils.ts
@@ -9,7 +9,8 @@ function loginRequest({username, password}: {username: string, password: string}
 
         const raw = JSON.stringify({username, password})
 
-        fetch(`${process.env.BE_URL}`, {
+
+        fetch(`/be/auth/login`, {
             method: "POST",
             headers: loginHeaders,
             body: raw
@@ -25,8 +26,26 @@ function loginRequest({username, password}: {username: string, password: string}
 
 }
 
-function signupRequest({username, password}: {username: string, password: string}){
+function signupRequest({username, password}: {username: string, password: string}): Promise<{created: boolean}>{
+    return new Promise((signupResolve, signupReject)=>{
+        
+        const signupHeaders = new Headers()
 
+        signupHeaders.append("Content-Type", "application/json");
+
+        const raw = JSON.stringify({username, password})
+
+        fetch(`/be/auth/signup`, {
+            method: "POST",
+            headers: signupHeaders,
+            body: raw
+        }).then((res)=>{
+            return signupResolve(res.json())
+        }, (err)=>{
+            signupReject(err)
+            console.log(err)
+        })
+    })
 }
 
-export {loginRequest}
+export {loginRequest, signupRequest}
