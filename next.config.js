a04d509072ef43dbf37940015898530273877d16
takam-alex-christian <takamalexchristian@gmail.com>
2024-02-07 19:20:09 +0800

    rewrites to redirect sll requests starting with /be to backend

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async rewrites(){
+        return [
+            {
+                "source": "/be/:path*", // the /be represents requests meant for the backend
+                "destination": `${process.env.BE_URL}/:path*`
+
+            }
+        ]
+    }
+}
 
 module.exports = nextConfig
