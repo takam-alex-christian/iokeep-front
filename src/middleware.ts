e1b43042df726a572402ff03e6659775d5eae18d
takam-alex-christian <takamalexchristian@gmail.com>
2024-02-11 22:31:08 +0800

    middleware reimplemented to authenticate a refresh token before opening app

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,20 +1,40 @@
 
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse, NextMiddleware } from "next/server";
 
 import { cookies } from "next/headers"
 
 
 
 
-export function middleware(req: NextRequest) {
+export async function middleware(req: NextRequest) {
+
+
     //check for authentication
-
+    // console.log(cookies().getAll())
     //check for validity of refresh token
-    if (!cookies().has("access_token")) {
+    if (cookies().has("refresh_token")) {
+        //verify refresh
+        const authHeaders = new Headers();
+        authHeaders.append("Authorization", `Bearer ${cookies().get("refresh_token")?.value}`)
 
+        try {
+            let authResponseData = await fetch(`${process.env.BE_URL}/auth/refresh_token`, {
+                headers: authHeaders,
+                method: "POST",
+            }).then((authResponse => authResponse.json()))
+
+            if (!authResponseData.isVerified || !authResponseData.isValid) return NextResponse.redirect(new URL("/login", req.url))
+            else return NextResponse.next()
+
+        } catch (err) {
+            console.log(err)
+        }
+
+    } else {
         return NextResponse.redirect(new URL("/login", req.url));
-
     }
+
+
 }
 
 export const config = {
