c0f7c140ff66460dbf0c087872b6e0d936f623c2
takam-alex-christian <takamalexchristian@gmail.com>
2024-02-15 19:45:41 +0800

    code refactoring

--- src/lib/noteUtils.ts
+++ src/lib/noteUtils.ts
@@ -6,32 +6,44 @@ import useSWR from "swr"
 // @ts-ignore
 const fetcher = (...args) => fetch(...args).then(res => res.json())
 
-async function createNote(noteData: {editorState: string, folderId: string}){
+async function createNote(noteData: { editorState: string, folderId: string }) {
 
     const cnHeaders = new Headers()
     cnHeaders.append("Content-Type", "application/json")
 
     const cnBody = JSON.stringify(noteData)
 
-    const jsonResponse: {success: boolean,data: {_id: string}, error: null | {message: string}, timeStamp: number} = await fetch(`be/notes`, {
+    const jsonResponse: { success: boolean, data: { _id: string }, error: null | { message: string }, timeStamp: number } = await fetch(`be/notes`, {
         method: "POST",
         headers: cnHeaders,
         body: cnBody
-    }).then((res)=> res.json())
+    }).then((res) => res.json())
 
     return jsonResponse
 }
 
-function useNotes(folderId: string){
+function useNotes(folderId: string) {
 
     /** 
      * data below is of type {error: {message: string} | null, data: Array<Reduced notes>, timeStamp}
     */
-   
-    const {data, error, isLoading} = useSWR(`be/notes?folderId=${folderId}`, fetcher)
+
+    const { data, error, isLoading } = useSWR(`be/notes?folderId=${folderId}`, fetcher)
 
     return {
-        notesData: (!isLoading && !error? data.data : []) as Array<NoteItemDataType>,
+        notesData: (!isLoading && !error ? data.data : []) as Array<NoteItemDataType>,
+        isLoading,
+        error
+    }
+
+}
+
+function useNote(noteId: string) {
+
+    const { data, error, isLoading } = useSWR(`be/notes/${noteId}`, fetcher);
+
+    return {
+        noteData: !isLoading && !error ? data.data : null,
         isLoading,
         error
     }
@@ -39,4 +51,6 @@ function useNotes(folderId: string){
 }
 
 
-export {createNote, useNotes}
+
+
+export { createNote, useNotes, useNote}
