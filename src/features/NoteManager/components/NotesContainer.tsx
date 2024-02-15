1fe49cd1a04e3828da9a537f351b107eb783848e
takam-alex-christian <takamalexchristian@gmail.com>
2024-02-15 16:49:54 +0800

    notecontainer component implemented to oversee notes fetching

---
+++ src/features/NoteManager/components/NotesContainer.tsx
@@ -0,0 +1,34 @@
+"use client"
+
+import { Divider } from "@nextui-org/react"
+
+import { liveDataContext } from "@/contexts/liveDataContext"
+import { useFolders } from "@/lib/folderUtils"
+import { useNotes } from "@/lib/noteUtils"
+import { useContext } from "react"
+
+import NoteItem from "./NoteItem"
+
+
+export default function () {
+
+
+    const {liveAppData} = useContext(liveDataContext)
+
+    const {notesData, isLoading, error} = useNotes(liveAppData.selectedFolderId!) //because this componenet renders its content only when folders are done loading
+    
+    return (
+        <div className="py-2 flex flex-col gap-0">
+            {!isLoading && notesData.map((eachNote, i) => {
+                return (
+                    <div key={eachNote._id}>
+                        <NoteItem key={eachNote._id} {...eachNote} />
+                        {notesData.length - 1 != i && <div className="px-3"><Divider className="bg-default/40" orientation="horizontal" /></div>}
+
+                    </div>
+                )
+            })}
+        </div>
+        // <></>
+    )
+}
