86e7e1af0be0fc9e3f5f7e585d8601f41d4ca181
takam-alex-christian <takamalexchristian@gmail.com>
2024-02-16 14:38:31 +0800

    note mutate on delete implemented

--- src/features/NoteManager/components/NoteToolBar.tsx
+++ src/features/NoteManager/components/NoteToolBar.tsx
@@ -9,10 +9,13 @@ import {faPlus} from "@fortawesome/free-solid-svg-icons"
 
 import {Button} from "@nextui-org/react"
 import { liveDataContext } from "@/contexts/liveDataContext"
+import { deleteNote, useNotes } from "@/lib/noteUtils"
+import { NoteItemDataType } from "@/types"
 
 export default function NoteToolBar(){
 
-    const {liveAppDataDispatch} = useContext(liveDataContext)
+    const {liveAppData, liveAppDataDispatch} = useContext(liveDataContext)
+    const {notesData, mutate} = useNotes()
 
     function addButtonHandler(){
         console.log("create new note button")
@@ -20,13 +23,40 @@ export default function NoteToolBar(){
     }
 
     function deleteButtonHandler(){
+        
+        if (liveAppData.selectedNoteId){
+            deleteNote(liveAppData.selectedNoteId).then((jsonResponse)=>{
+                if (!jsonResponse.error){
+                    if(jsonResponse.success) {
+                        // display something to infor the user that not is successfully deleted
+
+                        //mutate to remove selectedFolder id
+                        mutate(notesData.filter((eachNote)=>{return eachNote._id != liveAppData.selectedNoteId})).then((newData: Array<NoteItemDataType>)=>{
+                            if (newData.length > 0){
+                                liveAppDataDispatch({type: "changedSelectedNote", payload: {noteId: newData[0]._id}})
+                            }else {
+                                //inform user this folder is empty
+                            }
+                        })
+                        //select new not in the list
+
+                    }else{
+                        // inform the user that note was not deleted
+                    }
+                    console.log(jsonResponse.info)
+                }else{
+                    console.log(`Server found Error & says-> ${jsonResponse.error.message}}`)
+                }
+            })
+        }
+        console.log(liveAppData.selectedNoteId? "yes": "no")
         console.log("delete button pressed")
     }
     return (
         <div className="px-2">
             <div className="flex flex-row justify-between">
                 <Button color="success" isIconOnly onPress={addButtonHandler}><FontAwesomeIcon icon={faPlus} /></Button>
-                <Button  variant="light" onPress={deleteButtonHandler} isIconOnly><FontAwesomeIcon icon={faTrashCan} /></Button>
+                <Button  variant="light" onPress={deleteButtonHandler} isIconOnly isDisabled={liveAppData.selectedNoteId? false: true}><FontAwesomeIcon icon={faTrashCan} /></Button>
             </div>
         </div>
     )
