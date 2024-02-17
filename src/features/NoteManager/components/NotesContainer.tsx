a601d772d65fc963bfd82d01f1eeb5f2a44756ca
takam-alex-christian <takamalexchristian@gmail.com>
2024-02-17 15:02:02 +0800

    auto set selectedNote once they change

--- src/features/NoteManager/components/NotesContainer.tsx
+++ src/features/NoteManager/components/NotesContainer.tsx
@@ -5,16 +5,22 @@ import { Divider } from "@nextui-org/react"
 import { liveDataContext } from "@/contexts/liveDataContext"
 import { useFolders } from "@/lib/folderUtils"
 import { useNotes } from "@/lib/noteUtils"
-import { useContext } from "react"
+import { useContext, useEffect } from "react"
 
 import NoteItem from "./NoteItem"
 
 
 export default function () {
 
-
+    const {liveAppData, liveAppDataDispatch} = useContext(liveDataContext)
     const {notesData, isLoading, error} = useNotes() //because this componenet renders its content only when folders are done loading
     
+    useEffect(()=>{
+        if (!isLoading && notesData.length > 0){
+            liveAppDataDispatch({type: "changedSelectedNote", payload: {noteId: notesData[0]._id}})
+        }
+    }, [liveAppData.selectedFolderId, isLoading])
+
     return (
         <div className="py-2 flex flex-col gap-0">
             {!isLoading && notesData?.map((eachNote, i) => {
