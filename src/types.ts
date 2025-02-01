c0017c5f2946e5aea218899821b665fc84f6d1a3
takam-alex-christian <takamalexchristian@gmail.com>
2025-02-01 15:39:11 +0800

    folderDataType updated to include optional size?

--- src/types.ts
+++ src/types.ts
@@ -1,33 +1,40 @@
-
 //gobal types accessible accross features only
 
 type LiveDataState = {
-    selectedFolderId: string | null
-    selectedNoteId: string | null
-}
+  selectedFolderId: string | null;
+  selectedNoteId: string | null;
+};
 
-type LiveDataDispatchAction = {type: "changedSelectedFolder", payload: {folderId: string}} | {type: "changedSelectedNote", payload: {noteId: string}}
-
+type LiveDataDispatchAction =
+  | { type: "changedSelectedFolder"; payload: { folderId: string } }
+  | { type: "changedSelectedNote"; payload: { noteId: string } };
 
 type FolderDataType = {
-    _id: string,
-    folderName: string,
-    creationDate: string
-}
+  _id: string;
+  folderName: string;
+  creationDate: string;
+  size?: number;
+};
 
-
-type  AuthJsonResponse = { // all auth requests
-    success: boolean,
-    info: string,
-    error: null | {message: string}
-    timeStamp: number
-}
+type AuthJsonResponse = {
+  // all auth requests
+  success: boolean;
+  info: string;
+  error: null | { message: string };
+  timeStamp: number;
+};
 
 interface NoteItemDataType {
-    _id: string,
-    description: Array<string> // of size 2
-    creationDate: string,
-    lastModified: string
+  _id: string;
+  description: Array<string>; // of size 2
+  creationDate: string;
+  lastModified: string;
 }
 
-export type {LiveDataState, LiveDataDispatchAction, AuthJsonResponse, FolderDataType, NoteItemDataType}
+export type {
+  LiveDataState,
+  LiveDataDispatchAction,
+  AuthJsonResponse,
+  FolderDataType,
+  NoteItemDataType,
+};
