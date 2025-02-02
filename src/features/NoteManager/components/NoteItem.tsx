16cbe8e0e8f00ce3b1eec050d2f6e75d6e3bfe8f
takam-alex-christian <takamalexchristian@gmail.com>
2025-02-02 15:39:50 +0800

    last modified date and creation date  now displayed

--- src/features/NoteManager/components/NoteItem.tsx
+++ src/features/NoteManager/components/NoteItem.tsx
@@ -1,35 +1,66 @@
-"use client"
+"use client";
 
-import { useContext } from "react"
-import { Button } from "@nextui-org/react"
-import { liveDataContext } from "@/contexts/liveDataContext"
-import { NoteItemDataType } from "@/types"
+import { useContext } from "react";
+import { Button } from "@nextui-org/react";
+import { liveDataContext } from "@/contexts/liveDataContext";
+import { NoteItemDataType } from "@/types";
 
 export default function NoteItem(props: NoteItemDataType) {
+  const { liveAppData, liveAppDataDispatch } = useContext(liveDataContext);
 
-    const {liveAppData,liveAppDataDispatch} = useContext(liveDataContext)
+  const noteCreationDate = new Date(props.creationDate);
+  const noteLastModifiedDate = new Date(props.lastModifiedDate);
 
-    const noteCreationDate = new Date(props.creationDate)
+  const dateFormatter = new Intl.DateTimeFormat("en-US", {
+    weekday: "short",
+    month: "long",
+    day: "numeric",
+  });
 
-    function notePressHandler(){
-        liveAppDataDispatch({type: "changedSelectedNote", payload: {noteId: props._id}})
-    }
+  function notePressHandler() {
+    liveAppDataDispatch({
+      type: "changedSelectedNote",
+      payload: { noteId: props._id },
+    });
+  }
 
-    return (
-        <Button onPress={notePressHandler} key={props._id} className={`flex flex-row justify-start text-left h-fit px-0 py-2 w-full ${liveAppData.selectedNoteId == props._id? "bg-default/45" :"bg-transparent"}`}
-         >
-            <div className="flex flex-col gap-0 p-3 rounded-lg bg-none w-full ">
+  return (
+    <Button
+      onPress={notePressHandler}
+      key={props._id}
+      className={`flex flex-row justify-start text-left h-fit px-0 py-2 w-full ${
+        liveAppData.selectedNoteId == props._id
+          ? "bg-default/45"
+          : "bg-transparent"
+      }`}
+    >
+      <div className="flex flex-col gap-0 p-3 rounded-lg bg-none w-full ">
+        <div className="text-base py-0 font-semibold text-default-600 overflow-hidden overflow-ellipsis">
+          {props.description.length > 0
+            ? props.description[0]
+            : "no description"}
+        </div>
 
-                <div className="text-base py-0 font-semibold text-default-600 overflow-hidden overflow-ellipsis">{props.description.length > 0 ? props.description[0] : "no description"}</div>
-
-                <div className="flex flex-col gap-1 ">
-                    {/* {peak first p after heading} */}
-                    <p className="overflow-hidden overflow-ellipsis flex-grow text-default-400">{props.description.length >=2 ? props.description[1] : ""}</p>
-                    {/* date p */}
-                    <p className="text-sm text-default-400">{noteCreationDate.getDate()} {noteCreationDate.getMonth()} {noteCreationDate.getFullYear()}</p>
-                </div>
+        <div className="flex flex-col gap-1 ">
+          {/* {peak first p after heading} */}
+          <p className="overflow-hidden overflow-ellipsis flex-grow text-default-400">
+            {props.description.length >= 2 ? props.description[1] : ""}
+          </p>
+          {/* date p */}
+          <p className="text-sm text-default-400 flex flex-col gap-1">
+            {/* {noteCreationDate.getDate()} {noteCreationDate.getMonth()}{" "}
+            {noteCreationDate.getFullYear()} */}
+            <div>
+              <span className="font-semibold">Created </span>
+              {dateFormatter.format(noteCreationDate)}
             </div>
-
-        </Button>
-    )
+            <div>
+              <span className="font-semibold">Last modified </span>
+              {dateFormatter.format(noteLastModifiedDate)}
+            </div>
+          </p>
+        </div>
+      </div>
+    </Button>
+  );
 }
