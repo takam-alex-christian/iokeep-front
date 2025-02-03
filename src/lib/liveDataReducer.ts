07a1c4d82a5e41a250c3c68968137170e2f9d385
takam-alex-christian <takamalexchristian@gmail.com>
2025-02-03 18:31:10 +0800

    liveDataReducer updated to handler noteOrder change

--- src/lib/liveDataReducer.ts
+++ src/lib/liveDataReducer.ts
@@ -1,16 +1,20 @@
+import { LiveDataState, LiveDataDispatchAction } from "@/types";
 
-
-import {LiveDataState, LiveDataDispatchAction} from "@/types"
-
-export default function liveDataReducer(prevState: LiveDataState, action: LiveDataDispatchAction): LiveDataState{
-
-    switch (action.type){
-        case "changedSelectedFolder": {
-            return {...prevState, selectedFolderId: action.payload.folderId}
-        }
-        case "changedSelectedNote": {
-            return {...prevState, selectedNoteId: action.payload.noteId}
-        }
-        default: return prevState
+export default function liveDataReducer(
+  prevState: LiveDataState,
+  action: LiveDataDispatchAction
+): LiveDataState {
+  switch (action.type) {
+    case "changedSelectedFolder": {
+      return { ...prevState, selectedFolderId: action.payload.folderId };
     }
+    case "changedSelectedNote": {
+      return { ...prevState, selectedNoteId: action.payload.noteId };
+    }
+    case "changedNoteOrder": {
+      return { ...prevState, noteOrder: action.payload.newNoteOrder };
+    }
+    default:
+      return prevState;
+  }
 }
