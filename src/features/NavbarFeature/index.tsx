61949ef2d8be97800f4fb169249c5ae86ccf5664
takam-alex-christian <takamalexchristian@gmail.com>
2025-02-03 13:35:33 +0800

    theme toggle implemented

--- src/features/NavbarFeature/index.tsx
+++ src/features/NavbarFeature/index.tsx
@@ -1,53 +1,74 @@
+import Image from "next/image";
+import Link from "next/link";
 
-import Image from "next/image"
-import Link from "next/link"
+import V2Logo from "@/assets/logo.svg";
 
-import V2Logo from "@/assets/logo.svg"
+import { Button, Switch } from "@nextui-org/react";
 
-import { Button, Switch } from "@nextui-org/react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faRightFromBracket,
+  faCircleHalfStroke,
+} from "@fortawesome/free-solid-svg-icons";
+import { logoutRequest } from "@/lib/authUtils";
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faRightFromBracket, faCircleHalfStroke } from "@fortawesome/free-solid-svg-icons"
-import { logoutRequest } from "@/lib/authUtils"
-
-import { useRouter } from 'next/navigation'
+import { useRouter } from "next/navigation";
 
 export default function () {
+  const router = useRouter();
 
-    const router = useRouter()
+  function logoutButtonHandler() {
+    logoutRequest().then((jsonResponse) => {
+      if (jsonResponse.error) {
+        //maybe show a modal to communicate the error with user for improved user experience
+        alert(jsonResponse.error.message);
+      } else {
+        if (jsonResponse.success) {
+          router.replace("/login");
+        } else {
+          alert(jsonResponse.info);
+        }
+      }
+    });
+  }
 
-    function logoutButtonHandler() {
-        logoutRequest().then((jsonResponse) => {
-            if (jsonResponse.error) {
-                //maybe show a modal to communicate the error with user for improved user experience
-                alert(jsonResponse.error.message)
+  function selectedThemeChangeHandler() {
+    let currentTheme = window.localStorage.getItem("iokeepSelectedTheme");
+    let newPreferedTheme = currentTheme == "light" ? "dark" : "light";
 
-            } else {
+    window.localStorage.setItem("iokeepSelectedTheme", newPreferedTheme);
 
-                if (jsonResponse.success) {
-                    router.replace("/login")
-                } else {
-                    alert(jsonResponse.info)
-                }
-            }
-        })
-    }
+    if (currentTheme) document.body.classList.remove(currentTheme);
+    document.body.classList.add(newPreferedTheme);
+  }
 
-    return (
-        <header className="px-2 py-4 flex flex-row justify-center bg-white">
+  return (
+    <header className="px-2 py-4 flex flex-row justify-center bg-white">
+      <div className="w-4/5 flex flex-row justify-between items-center ">
+        {/* logo */}
+        <Link href={"/app"}>
+          <Image width={48} src={V2Logo} alt={"iokeep v2 logo"} />
+        </Link>
 
-            <div className="w-4/5 flex flex-row justify-between items-center ">
-                {/* logo */}
-                <Link href={"/app"}>
-                    <Image width={48} src={V2Logo} alt={"iokeep v2 logo"} />
-                </Link>
-
-
-                <div className="flex flex-row justify-center items-center gap-2">
-                    <Button isIconOnly variant="flat" color="default"><FontAwesomeIcon icon={faCircleHalfStroke} /></Button>
-                    <Button onPress={logoutButtonHandler} variant="flat" color="default" isIconOnly><FontAwesomeIcon icon={faRightFromBracket} /></Button>
-                </div>
-            </div>
-        </header>
-    )
+        <div className="flex flex-row justify-center items-center gap-2">
+          <Button
+            onPress={selectedThemeChangeHandler}
+            isIconOnly
+            variant="flat"
+            color="default"
+          >
+            <FontAwesomeIcon icon={faCircleHalfStroke} />
+          </Button>
+          <Button
+            onPress={logoutButtonHandler}
+            variant="flat"
+            color="default"
+            isIconOnly
+          >
+            <FontAwesomeIcon icon={faRightFromBracket} />
+          </Button>
+        </div>
+      </div>
+    </header>
+  );
 }
