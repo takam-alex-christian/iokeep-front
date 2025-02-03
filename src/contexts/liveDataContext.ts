efc231203624d3a54322be710db3ad1638dcd6d3
takam-alex-christian <takamalexchristian@gmail.com>
2025-02-03 18:29:22 +0800

    livedatacontext updated ton initialize noteOrder

--- src/contexts/liveDataContext.ts
+++ src/contexts/liveDataContext.ts
@@ -1,21 +1,19 @@
+import { createContext, Dispatch } from "react";
 
-
-import {createContext, Dispatch} from "react"
-
-import type {LiveDataState, LiveDataDispatchAction} from "@/types"
+import type { LiveDataState, LiveDataDispatchAction } from "@/types";
 
 const initialLiveData: LiveDataState = {
-    selectedFolderId: null,
-    selectedNoteId: null
-}
+  selectedFolderId: null,
+  selectedNoteId: null,
+  noteOrder: "lmd",
+};
 
 const liveDataContext = createContext<{
-    liveAppData: LiveDataState,
-    liveAppDataDispatch: Dispatch<LiveDataDispatchAction>
+  liveAppData: LiveDataState;
+  liveAppDataDispatch: Dispatch<LiveDataDispatchAction>;
 }>({
-    liveAppData: initialLiveData,
-    liveAppDataDispatch: ()=>{}
-})
+  liveAppData: initialLiveData,
+  liveAppDataDispatch: () => {},
+});
 
-
-export {liveDataContext, initialLiveData}
+export { liveDataContext, initialLiveData };
