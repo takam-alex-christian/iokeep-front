b4be84b4dbadfdd71dd9eb02d4a085fde84fe28a
takam-alex-christian <takamalexchristian@gmail.com>
2025-02-07 22:13:53 +0800

    code refactoring on sigup and login forms

--- src/features/LoginForm/index.tsx
+++ src/features/LoginForm/index.tsx
@@ -1,186 +1,176 @@
-"use client"
+"use client";
 
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useState } from "react";
 
-import { Input, Button, Link } from "@heroui/react"
+import { Input, Button, Link } from "@heroui/react";
 
-import { loginRequest } from "@/lib/authUtils"
+import { loginRequest } from "@/lib/authUtils";
 
-import { useRouter } from "next/navigation"
+import { useRouter } from "next/navigation";
 
 interface FormTextInputType {
-    value: string,
-    isInvalid: boolean,
-    errorMessage: string,
-    isEngaged: boolean
+  value: string;
+  isInvalid: boolean;
+  errorMessage: string;
+  isEngaged: boolean;
 }
 
 interface FormState {
-    username: FormTextInputType,
-    password: FormTextInputType
+  username: FormTextInputType;
+  password: FormTextInputType;
 }
 
 export default function () {
+  const initialTextInputData = {
+    value: "",
+    isInvalid: false,
+    errorMessage: "",
+    isEngaged: false,
+  };
 
-    const initialTextInputData = {
-        value: "",
-        isInvalid: false,
-        errorMessage: "",
-        isEngaged: false
+  const [formState, setFormState] = useState<FormState>({
+    username: initialTextInputData,
+    password: initialTextInputData,
+  });
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (formState.username.isEngaged) validateUsername();
+
+    if (formState.password.isEngaged) validatePassword();
+  }, [formState.username.value, formState.password.value]);
+
+  function usernameChangeHandler(value: string) {
+    setFormState((prevState) => {
+      return {
+        ...prevState,
+        username: { ...prevState.username, value, isEngaged: true },
+      };
+    });
+  }
+
+  function validateUsername() {
+    let isInValid: boolean = false;
+    let errorMessage: string = "";
+
+    if (formState.username.value.length == 0) {
+      isInValid = true;
+      errorMessage = "Username can not be empty!";
     }
 
-    const [formState, setFormState] = useState<FormState>({
-        username: initialTextInputData,
-        password: initialTextInputData
+    setFormState((prevState) => {
+      return {
+        ...prevState,
+        username: { ...prevState.username, isInValid, errorMessage },
+      };
+    });
+  }
+
+  function passwordChangeHandler(value: string) {
+    setFormState((prevState) => {
+      return {
+        ...prevState,
+        password: { ...prevState.password, value, isEngaged: true },
+      };
+    });
+  }
+
+  function validatePassword() {
+    let isInValid: boolean = false;
+    let errorMessage: string = "";
+
+    if (formState.password.value.length == 0) {
+      isInValid = true;
+      errorMessage = "password can not be empty!";
+    }
+
+    setFormState((prevState) => {
+      return {
+        ...prevState,
+        password: { ...prevState.password, isInValid, errorMessage },
+      };
+    });
+  }
+
+  function formSubmitHandler(e: React.FormEvent) {
+    e.preventDefault();
+
+    //validate inputs
+
+    //initiate request
+    loginRequest({
+      username: formState.username.value,
+      password: formState.password.value,
     })
+      .then((jsonResponse) => {
+        if (process.env.NODE_ENV === "development")
+          console.log(`loginRequest Response ${jsonResponse}`);
 
-    const router = useRouter()
-
-    useEffect(() => {
-
-        if (formState.username.isEngaged) validateUsername()
-
-        if (formState.password.isEngaged) validatePassword()
-
-    }, [formState.username.value, formState.password.value])
-
-
-    function usernameChangeHandler(value: string) {
-
-        setFormState((prevState) => {
-            return { ...prevState, username: { ...prevState.username, value, isEngaged: true } }
-        })
-
-    }
-
-    function validateUsername() {
-
-        let isInValid: boolean = false
-        let errorMessage: string = ""
-
-        if (formState.username.value.length == 0) {
-            isInValid = true;
-            errorMessage = "Username can not be empty!"
+        if (jsonResponse.error) {
+          alert(jsonResponse.error.message);
+        } else {
+          if (jsonResponse.success) {
+            router.replace("/app");
+          } else {
+            alert(jsonResponse.info);
+          }
         }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  return (
+    <div className="p-6 w-96 flex flex-col gap-0">
+      <form onSubmit={formSubmitHandler}>
+        <div className="flex flex-col gap-4">
+          <div className="flex flex-col gap-2">
+            <Input
+              key={0}
+              value={formState.username.value}
+              onValueChange={usernameChangeHandler}
+              // className="focus-within:outline-dashed focus-within:outline-8 focus-within:outline-red-700"
 
-        setFormState((prevState) => {
-            return { ...prevState, username: { ...prevState.username, isInValid, errorMessage, } }
-        })
-    }
-
-    function passwordChangeHandler(value: string) {
-        setFormState((prevState) => {
-            return { ...prevState, password: { ...prevState.password, value, isEngaged: true } }
-        })
-    }
-
-    function validatePassword() {
-        let isInValid: boolean = false
-        let errorMessage: string = ""
-
-        if (formState.password.value.length == 0) {
-            isInValid = true;
-            errorMessage = "password can not be empty!"
-        }
-
-        setFormState((prevState) => {
-            return { ...prevState, password: { ...prevState.password, isInValid, errorMessage } }
-        })
-    }
-
-
-    function formSubmitHandler(e: React.FormEvent) {
-        e.preventDefault()
-
-        //validate inputs
-
-
-        //initiate request
-        loginRequest({ username: formState.username.value, password: formState.password.value }).then((jsonResponse) => {
-
-            if (process.env.NODE_ENV === 'development') console.log(`loginRequest Response ${jsonResponse}`)
-
-            if (jsonResponse.error) {
-
-                alert(jsonResponse.error.message)
-
-            } else {
-                if (jsonResponse.success) {
-                    router.replace("/app")
-
-                } else {
-                    alert(jsonResponse.info)
-                }
-            }
-
-        }).catch((err) => {
-            console.log(err)
-        })
-
-    }
-    return (
-        <div className="p-6 w-96 flex flex-col gap-0">
-            <form onSubmit={formSubmitHandler}>
-                <div className="flex flex-col gap-4">
-                    <div className="flex flex-col gap-2">
-                        <Input
-                            key={0}
-
-                            value={formState.username.value}
-                            onValueChange={usernameChangeHandler}
-
-                            // className="focus-within:outline-dashed focus-within:outline-8 focus-within:outline-red-700"
-
-                            type="text"
-                            name="username"
-
-
-                            placeholder={"Username"}
-                            isRequired
-
-                            isInvalid={formState.username.isInvalid}
-                            errorMessage={formState.username.errorMessage}
-
-                            size="sm"
-
-                            autoFocus
-                            autoComplete="username"
-
-                        />
-                        <div className="flex flex-row justify-end pt-2 pb-0">
-                            <Link href={"#"}>password forgoten ?</Link>
-                        </div>
-                        <Input
-
-                            key={1}
-
-                            value={formState.password.value}
-                            onValueChange={passwordChangeHandler}
-
-                            type={"password"}
-                            name="password"
-
-                            placeholder={"Password"}
-
-                            isRequired
-
-                            isInvalid={formState.password.isInvalid}
-                            errorMessage={formState.password.errorMessage}
-
-                            size="sm"
-
-                            autoComplete="password"
-                        />
-                    </div>
-
-                    <Button size="lg" color="primary" variant="solid" type="submit">Log in</Button>
-
-                </div>
-            </form>
-            <div className="flex flex-row justify-center items-center gap-2 px-2 py-4">
-                <div>New to iokeep?</div>
-                <Link href={"/signup"} showAnchorIcon={true}>Sign up</Link>
+              type="text"
+              name="username"
+              placeholder={"Username"}
+              isRequired
+              isInvalid={formState.username.isInvalid}
+              errorMessage={formState.username.errorMessage}
+              size="lg"
+              autoFocus
+              autoComplete="username"
+            />
+            <div className="flex flex-row justify-end pt-2 pb-0">
+              <Link href={"#"}>password forgoten ?</Link>
             </div>
+            <Input
+              key={1}
+              value={formState.password.value}
+              onValueChange={passwordChangeHandler}
+              type={"password"}
+              name="password"
+              placeholder={"Password"}
+              isRequired
+              isInvalid={formState.password.isInvalid}
+              errorMessage={formState.password.errorMessage}
+              size="lg"
+              autoComplete="password"
+            />
+          </div>
+
+          <Button size="lg" color="primary" variant="solid" type="submit">
+            Log in
+          </Button>
         </div>
-    )
+      </form>
+      <div className="flex flex-row justify-center items-center gap-2 px-2 py-4">
+        <div>New to iokeep?</div>
+        <Link href={"/signup"} showAnchorIcon={true}>
+          Sign up
+        </Link>
+      </div>
+    </div>
+  );
 }
--- src/features/SignupForm/index.tsx
+++ src/features/SignupForm/index.tsx
@@ -1,230 +1,236 @@
-"use client"
+"use client";
 
-import React, { useState, useRef, useEffect } from "react"
-import { Input, Button, Link, user } from "@heroui/react"
-import { signupRequest } from "@/lib/authUtils"
+import React, { useState, useRef, useEffect } from "react";
+import { Input, Button, Link, user } from "@heroui/react";
+import { signupRequest } from "@/lib/authUtils";
 
-import { useRouter } from "next/navigation"
+import { useRouter } from "next/navigation";
 
 interface FormState {
-    username: {
-        value: string,
-        isInValid: boolean,
-        errorMessage: string,
-        isEngaged: boolean //can only start validating a component if it has been engaged
-    },
-    password: {
-        value: string,
-        isInValid: boolean,
-        errorMessage: string,
-        isEngaged: boolean
-    },
-    confirmPassword: {
-        value: string,
-        isInValid: boolean,
-        errorMessage: string,
-        isEngaged: boolean
-    }
+  username: {
+    value: string;
+    isInValid: boolean;
+    errorMessage: string;
+    isEngaged: boolean; //can only start validating a component if it has been engaged
+  };
+  password: {
+    value: string;
+    isInValid: boolean;
+    errorMessage: string;
+    isEngaged: boolean;
+  };
+  confirmPassword: {
+    value: string;
+    isInValid: boolean;
+    errorMessage: string;
+    isEngaged: boolean;
+  };
 }
 
-
 export default function () {
+  const [formState, setFormState] = useState<FormState>({
+    username: {
+      value: "",
+      isInValid: false,
+      errorMessage: "",
+      isEngaged: false,
+    },
+    password: {
+      value: "",
+      isInValid: false,
+      errorMessage: "",
+      isEngaged: false,
+    },
+    confirmPassword: {
+      value: "",
+      isInValid: false,
+      errorMessage: "",
+      isEngaged: false,
+    },
+  });
 
-    const [formState, setFormState] = useState<FormState>({
-        username: {
-            value: "",
-            isInValid: false,
-            errorMessage: "",
-            isEngaged: false
-        },
-        password: {
-            value: "",
-            isInValid: false,
-            errorMessage: "",
-            isEngaged: false,
+  const router = useRouter();
 
-        },
+  useEffect(() => {
+    if (formState.username.isEngaged) validateUsername();
+
+    if (formState.password.isEngaged) validatePassword();
+
+    if (formState.confirmPassword.isEngaged) validateConfirmPassword();
+  }, [
+    formState.username.value,
+    formState.password.value,
+    formState.confirmPassword.value,
+  ]);
+
+  function usernameChangeHandler(value: string) {
+    setFormState((prevState: FormState) => {
+      return {
+        ...prevState,
+        username: { ...prevState.username, value, isEngaged: true },
+      };
+    });
+  }
+
+  function validateUsername() {
+    let isInValid: boolean = false;
+    let errorMessage: string = "";
+
+    if (formState.username.value.length == 0) {
+      isInValid = true;
+      errorMessage = "Username can not be empty!";
+    }
+
+    setFormState((prevState) => {
+      return {
+        ...prevState,
+        username: { ...prevState.username, isInValid, errorMessage },
+      };
+    });
+  }
+
+  function passwordChangeHandler(value: string) {
+    setFormState((prevState: FormState) => {
+      return {
+        ...prevState,
+        password: { ...prevState.password, value, isEngaged: true },
+      };
+    });
+  }
+
+  function validatePassword() {
+    let isInValid: boolean = false;
+    let errorMessage: string = "";
+
+    if (formState.password.value.length == 0) {
+      isInValid = true;
+      errorMessage = "password can not be empty!";
+    }
+
+    setFormState((prevState) => {
+      return {
+        ...prevState,
+        password: { ...prevState.password, isInValid, errorMessage },
+      };
+    });
+  }
+
+  function confirmPasswordChangeHandler(value: string) {
+    setFormState((prevState: FormState) => {
+      return {
+        ...prevState,
         confirmPassword: {
-            value: "",
-            isInValid: false,
-            errorMessage: "",
-            isEngaged: false,
-        }
+          ...prevState.confirmPassword,
+          value,
+          isEngaged: true,
+        },
+      };
+    });
+  }
+
+  function validateConfirmPassword() {
+    let isInValid: boolean = false;
+    let errorMessage: string = "";
+
+    if (formState.confirmPassword.value.length > 0) {
+      if (formState.password.value != formState.confirmPassword.value) {
+        isInValid = true;
+        errorMessage = "Both passwords don't match";
+      }
+    } else {
+      isInValid = true;
+      errorMessage = "password can not be empty!";
+    }
+
+    setFormState((prevState) => {
+      return {
+        ...prevState,
+        confirmPassword: {
+          ...prevState.confirmPassword,
+          isInValid,
+          errorMessage,
+        },
+      };
+    });
+  }
+
+  function formSubmitHandler(e: React.FormEvent) {
+    e.preventDefault();
+
+    //activate a loading state
+
+    signupRequest({
+      username: formState.username.value,
+      password: formState.password.value,
     })
-
-    const router = useRouter()
-
-    useEffect(() => {
-
-        if (formState.username.isEngaged) validateUsername()
-
-        if (formState.password.isEngaged) validatePassword()
-
-        if (formState.confirmPassword.isEngaged) validateConfirmPassword()
-
-    }, [formState.username.value, formState.password.value, formState.confirmPassword.value])
-
-    function usernameChangeHandler(value: string) {
-
-        setFormState((prevState: FormState) => { return { ...prevState, username: { ...prevState.username, value, isEngaged: true } } })
-
-    }
-
-    function validateUsername() {
-
-        let isInValid: boolean = false
-        let errorMessage: string = ""
-
-        if (formState.username.value.length == 0) {
-            isInValid = true;
-            errorMessage = "Username can not be empty!"
+      .then((jsonResponse) => {
+        if (jsonResponse.error) alert(jsonResponse.error.message);
+        else {
+          if (jsonResponse.success) router.push("/login");
+          //communicate to the user that they just singed up and should login to access app
+          else alert(jsonResponse.info);
         }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
-        setFormState((prevState) => {
-            return { ...prevState, username: { ...prevState.username, isInValid, errorMessage, } }
-        })
+  return (
+    <div className="p-6 w-96">
+      <form onSubmit={formSubmitHandler}>
+        <div className="flex flex-col gap-4">
+          <div className="flex flex-col gap-2">
+            <Input
+              key={0}
+              value={formState.username.value}
+              onValueChange={usernameChangeHandler}
+              placeholder={"Username"}
+              isRequired
+              isInvalid={formState.username.isInValid}
+              errorMessage={formState.username.errorMessage}
+              size="lg"
+              autoFocus={true}
+              autoComplete={"off"}
+            />
+            <Input
+              key={1}
+              value={formState.password.value}
+              onValueChange={passwordChangeHandler}
+              placeholder={"Password"}
+              type="password"
+              isRequired
+              isInvalid={formState.password.isInValid}
+              errorMessage={formState.password.errorMessage}
+              size="lg"
+              autoComplete="off"
+            />
 
-    }
+            <Input
+              key={2}
+              value={formState.confirmPassword.value}
+              onValueChange={confirmPasswordChangeHandler}
+              placeholder={"Confirm Password"}
+              type={"password"}
+              isRequired
+              isInvalid={formState.confirmPassword.isInValid}
+              errorMessage={formState.confirmPassword.errorMessage}
+              size="lg"
+              autoComplete="off"
+            />
+          </div>
 
-    function passwordChangeHandler(value: string) {
-
-
-        setFormState((prevState: FormState) => { return { ...prevState, password: { ...prevState.password, value, isEngaged: true } } })
-    }
-
-    function validatePassword() {
-        let isInValid: boolean = false
-        let errorMessage: string = ""
-
-        if (formState.password.value.length == 0) {
-            isInValid = true;
-            errorMessage = "password can not be empty!"
-        }
-
-        setFormState((prevState) => {
-            return { ...prevState, password: { ...prevState.password, isInValid, errorMessage } }
-        })
-    }
-
-    function confirmPasswordChangeHandler(value: string) {
-
-        setFormState((prevState: FormState) => { return { ...prevState, confirmPassword: { ...prevState.confirmPassword, value, isEngaged: true } } })
-    }
-
-    function validateConfirmPassword() {
-        let isInValid: boolean = false
-        let errorMessage: string = ""
-
-
-        if (formState.confirmPassword.value.length > 0) {
-            if (formState.password.value != formState.confirmPassword.value) {
-                isInValid = true;
-                errorMessage = "Both passwords don\'t match"
-            }
-        } else {
-            isInValid = true;
-            errorMessage = "password can not be empty!"
-        }
-
-        setFormState((prevState) => {
-            return { ...prevState, confirmPassword: { ...prevState.confirmPassword, isInValid, errorMessage } }
-        })
-
-
-    }
-
-    function formSubmitHandler(e: React.FormEvent) {
-
-        e.preventDefault()
-
-        //activate a loading state
-
-        signupRequest({ username: formState.username.value, password: formState.password.value }).then((jsonResponse) => {
-
-            if (jsonResponse.error) alert(jsonResponse.error.message)
-            else {
-
-                if (jsonResponse.success) router.push("/login") //communicate to the user that they just singed up and should login to access app
-                else alert(jsonResponse.info)
-        
-            }
-
-        }).catch((err) => {
-            console.log(err)
-        })
-
-    }
-
-
-    return (
-        <div className="p-6 w-96">
-            <form onSubmit={formSubmitHandler}>
-                <div className="flex flex-col gap-4">
-                    <div className="flex flex-col gap-2">
-                        <Input
-                            key={0}
-
-                            value={formState.username.value}
-                            onValueChange={usernameChangeHandler}
-
-                            placeholder={"Username"}
-                            isRequired
-
-                            isInvalid={formState.username.isInValid}
-                            errorMessage={formState.username.errorMessage}
-
-                            size="sm"
-
-                            autoFocus={true}
-                            autoComplete={"off"}
-                        />
-                        <Input
-                            key={1}
-                            value={formState.password.value}
-                            onValueChange={passwordChangeHandler}
-
-                            placeholder={"Password"}
-
-                            type="password"
-                            isRequired
-
-                            isInvalid={formState.password.isInValid}
-                            errorMessage={formState.password.errorMessage}
-                            size="sm"
-
-                            autoComplete="off"
-                        />
-
-                        <Input
-                            key={2}
-
-                            value={formState.confirmPassword.value}
-                            onValueChange={confirmPasswordChangeHandler}
-
-                            placeholder={"Confirm Password"}
-
-                            type={"password"}
-                            isRequired
-
-                            isInvalid={formState.confirmPassword.isInValid}
-                            errorMessage={formState.confirmPassword.errorMessage}
-
-                            size="sm"
-
-
-                            autoComplete="off"
-                        />
-
-                    </div>
-
-                    <Button type="submit" size="lg" color="primary" variant="solid">Sign Up</Button>
-
-                </div>
-            </form>
-            <div className="flex flex-row gap-2 px-2 py-4 justify-center items-center">
-                <div>Already have an account?</div>
-                <Link href={"/login"} showAnchorIcon={true}>Log in</Link>
-            </div>
+          <Button type="submit" size="lg" color="primary" variant="solid">
+            Sign Up
+          </Button>
         </div>
-    )
+      </form>
+      <div className="flex flex-row gap-2 px-2 py-4 justify-center items-center">
+        <div>Already have an account?</div>
+        <Link href={"/login"} showAnchorIcon={true}>
+          Log in
+        </Link>
+      </div>
+    </div>
+  );
 }
