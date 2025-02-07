19d67714959e0db60172e98aac8090da4749901e
takam-alex-christian <takamalexchristian@gmail.com>
2025-02-07 21:36:53 +0800

    bug fixes on screen height

--- src/app/notes/[noteId]/page.tsx
+++ src/app/notes/[noteId]/page.tsx
@@ -1,5 +1,6 @@
 import NavbarFeature from "@/features/NavbarFeature";
 import { ReadOnlyEditor } from "@/features/NoteEditor";
+import Footer from "@/layouts/Footer";
 import { NoteItemDataType } from "@/types";
 
 export default async function NoteReaderPage({
@@ -19,11 +20,14 @@ export default async function NoteReaderPage({
       }
     });
   return (
-    <main className="custom-bg-image flex flex-col gap-6 h-screen overflow-hidden">
+    <main className="custom-bg-image flex flex-col gap-6 h-svh overflow-hidden">
       <NavbarFeature />
-      <div>
-        <ReadOnlyEditor {...noteData!} isEditable={false} />
+      <div className="flex flex-col w-full h-full items-center">
+        <div className="max-w-screen-lg w-full ">
+          <ReadOnlyEditor {...noteData!} isEditable={false} />
+        </div>
       </div>
+      <Footer />
     </main>
   );
 }
