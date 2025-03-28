73f071f65e6d2c1a072245d75e68ed5db1602467
takam-alex-christian <takamalexchristian@gmail.com>
2025-03-28 13:43:57 +0800

    code refactoring. get access token before iterval.

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -9,24 +9,33 @@ import NavbarFeature from "@/features/NavbarFeature";
 import { getAccessToken } from "@/lib/authUtils";
 import { useEffect } from "react";
 
+
+function getAccessTokenFuction(){
+  getAccessToken()
+  .then((jsonResponse) => {
+    console.log(jsonResponse)
+    if (jsonResponse.error) console.log(jsonResponse.error.message);
+    else {
+      if (!jsonResponse.success) console.log(jsonResponse.info);
+    }
+    console.log("new auth token received");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+}
+
 export default function LandingPage() {
   //verify token and query new token at every interval before token expire
   //access_token lifetime is 10 minutes
 
   useEffect(() => {
+    
+    getAccessTokenFuction()
+
     const gati = setInterval(() => {
       //get access token interval
-      getAccessToken()
-        .then((jsonResponse) => {
-          if (jsonResponse.error) console.log(jsonResponse.error.message);
-          else {
-            if (!jsonResponse.success) console.log(jsonResponse.info);
-          }
-          console.log("new auth token received");
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+      getAccessTokenFuction()
     }, 59000); // this time corresponds to the validity period of an access token, ideally should be a few tens of seconds lower than the actual token validity period
 
     return () => {
